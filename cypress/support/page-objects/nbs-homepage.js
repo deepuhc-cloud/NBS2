35f2197ee72896e37111c08217f30e33da06879e
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2025-07-14 13:56:38 +0100

    added small wait to image snapshot test, all tests working fine

--- cypress/support/page-objects/nbs-homepage.js
+++ cypress/support/page-objects/nbs-homepage.js
@@ -46,7 +46,7 @@ acceptCookies() {
         cy.viewport(1000, 4410); // Set a fixed viewport size to match the baseline snapshot
         cy.wait(2000); // Wait for 2 seconds to ensure the site has loaded and dynamic content is rendered
         cy.scrollTo('bottom'); // Scroll to the bottom to ensure all content is rendered
-        cy.wait(500); // Wait a bit after scrolling
+        cy.wait(1000); // Wait a bit after scrolling
         cy.matchImageSnapshot('dyson-homepage', {
             failureThreshold: 0.40, // Allow up to 40% difference
             failureThresholdType: 'percent',
