559ff8fb69385ababc0cb4f90d154db2cf6586aa
deepuhc-cloud <deepuhc@gmail.com>
2025-08-08 10:35:17 +0100

    Run3

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,45 @@
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
 const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const webpack = require('@cypress/webpack-preprocessor');
 
 module.exports = async (on, config) => {
+  // Add image snapshot plugin
   addMatchImageSnapshotPlugin(on, config);
+  
+  // Add cucumber preprocessor plugin
   await addCucumberPreprocessorPlugin(on, config);
+  
+  // Add log task for accessibility tests
+  on('task', {
+    log(message) {
+      console.log(message);
+      return null;
+    }
+  });
+  
+  // Configure webpack preprocessor for cucumber
+  const options = {
+    webpackOptions: {
+      resolve: {
+        extensions: ['.ts', '.js']
+      },
+      module: {
+        rules: [
+          {
+            test: /\.feature$/,
+            use: [
+              {
+                loader: '@badeball/cypress-cucumber-preprocessor/webpack',
+                options: config
+              }
+            ]
+          }
+        ]
+      }
+    }
+  };
+  
+  on('file:preprocessor', webpack(options));
+  
   return config;
 };
